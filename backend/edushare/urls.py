abc963727841a51daf14657c0fd5efc01796c94d
Chinyere <chinyereunamba17@gmail.com>
2024-07-17 17:19:30 +0100

    backend: renamed root folder

--- backend/edushare/urls.py
+++ backend/edushare/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for educare project.
+URL configuration for edushare project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
--- backend/edushare/wsgi.py
+++ backend/edushare/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for educare project.
+WSGI config for edushare project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educare.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edushare.settings")
 
 application = get_wsgi_application()
